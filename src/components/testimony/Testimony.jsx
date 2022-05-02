ff6d9a0e8e5bf33f2bace7202ad0f6dbcf0b538e
oluwadaprof <adeekotobiloba8@gmail.com>
2022-05-02 11:53:23 +0100

    another error

--- src/components/testimony/Testimony.jsx
+++ src/components/testimony/Testimony.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import p1 from '../../ASSETS/p1.jpg'
+
 import './testimony.css'
 
-const Testimony = ({image, name, status, children}) => {
+const Testimony = ({rep, name, status, children}) => {
   return (
     <div className='permuta_testimony'>
-         <img className='permuta_testimony-img' src={image} alt="testimony" />
+         <img className='permuta_testimony-img' src={rep} alt="testimony" />
         <div className='permuta_testimony-cnt2'>
             <p className='permuta_testimony-p1' > {name} </p>
             <p className='permuta_testimony-p2'> {status} </p>
