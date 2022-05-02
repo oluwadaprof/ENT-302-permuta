6fbd4f7c323c5ce8410cfe4f119a3132a9283beb
oluwadaprof <adeekotobiloba8@gmail.com>
2022-05-02 12:01:08 +0100

    fix image error

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -12,21 +12,21 @@ const Review = () => {
         <div>
             <h1 className='review-container-h1'>Read What Our Customer Say</h1>
             <div className='review-container-cnt2'>
-            <Testimony name='Njoku Ifeanyi' status='COO Permuta' image={ify2}>
+            <Testimony name='Njoku Ifeanyi' status='COO Permuta' rep={ify2}>
                 <p>Designing and implementing buisness operations. Establishing policies that promote company culture and vision. 
                     Overseeing operations of the company and the work of executives.</p>
             </Testimony>
-            <Testimony name='Celia Almedia' status='CEO Mailcharm'image={p5} >
+            <Testimony name='Celia Almedia' status='CEO Mailcharm' rep={p5} >
                 <p>Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. 
                     Ut sem nulla pharetra diam sit amet nisl. Enim nunc faucibus a pellentesque sit amet.
                     Sed turpis tincidunt id aliquet risus feugiat in ante metus.</p>
             </Testimony>
-            <Testimony name='Celia Almedia' status='CEO Mailcharm' image={p3}>
+            <Testimony name='Celia Almedia' status='CEO Mailcharm' rep={p3}>
                 <p>Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. 
                     Ut sem nulla pharetra diam sit amet nisl. Enim nunc faucibus a pellentesque sit amet.
                     Sed turpis tincidunt id aliquet risus feugiat in ante metus.</p>
             </Testimony>
-            <Testimony name='Celia Almedia' status='CEO Mailcharm' image={p6}>
+            <Testimony name='Celia Almedia' status='CEO Mailcharm' rep={p6}>
                 <p>Proin sed libero enim sed faucibus turpis. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. 
                     Ut sem nulla pharetra diam sit amet nisl. Enim nunc faucibus a pellentesque sit amet.
                     Sed turpis tincidunt id aliquet risus feugiat in ante metus.</p>
